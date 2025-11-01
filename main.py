fd53fff64ddbd74c56b5a1107dbeb098ea2ed341
SIDDHARTH K P <siddhubhai998@gmail.com>
2025-11-02 00:06:06 +0530

    Avoid colon to prevent confusion while parsing ID.

--- main.py
+++ main.py
@@ -25,10 +25,11 @@ class MainWindow(QMainWindow):
         plain_text_bytes = plain_text.encode('utf-8')
         encrypted_blob = cipher.encrypt(plain_text_bytes)
         return encrypted_blob, key
-        
+
     @staticmethod
     def random_string(length: int =  7) -> str:
         characters = string.ascii_letters + string.digits + string.punctuation
+        characters = characters.replace(':', '')
         return ''.join(random.choice(characters) for _ in range(length))
     
     def on_send_clicked(self):
