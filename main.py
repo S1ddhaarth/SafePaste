efed135de8921a0baabe375c2e638a009f826447
SIDDHARTH K P <siddhubhai998@gmail.com>
2025-11-08 12:22:57 +0530

    Defined conditions to load the MainWindow and ConfigWindow inside the main.py file.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
+import os
 from PyQt6.QtWidgets import QApplication
-from app_window import MainWindow
 from config_window import ConfigWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = ConfigWindow()
+    if os.path.isfile("config.txt") and os.path.isfile("serviceAccountKey.json"):
+        from app_window import MainWindow
+        window = MainWindow()
+    else:
+        window = ConfigWindow()
     window.show()
     sys.exit(app.exec())
